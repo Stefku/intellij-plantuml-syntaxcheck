c27d5b8fa63d2a6052fca23eb639b172ec519df9
Stefan Zeller <stefan.zeller@docksnet.de>
2013-01-24 13:02:41 +0100

    refactor scanning

--- src/de/docksnet/puml/PlantUmlExternalAnnotator.java
+++ src/de/docksnet/puml/PlantUmlExternalAnnotator.java
@@ -23,6 +23,18 @@ import java.util.Scanner;
 public class PlantUmlExternalAnnotator extends ExternalAnnotator<String, SyntaxResult> {
 
     public static final int SIZE_OF_EOL_CHAR = 1;
+    public static final String TAG_STARTUML = "@startuml";
+    private static final Segment EMPTY_SEGMENT = new Segment() {
+        @Override
+        public int getStartOffset() {
+            return -1;
+        }
+
+        @Override
+        public int getEndOffset() {
+            return -1;
+        }
+    };
 
     @Nullable
     @Override
@@ -33,7 +45,10 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<String, SyntaxR
     @Nullable
     @Override
     public SyntaxResult doAnnotate(String collectedInfo) {
-        return SyntaxChecker.checkSyntax(collectedInfo);
+        if (collectedInfo.contains(TAG_STARTUML)) {
+            return SyntaxChecker.checkSyntax(collectedInfo);
+        }
+        return null;
     }
 
     @Override
@@ -42,6 +57,11 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<String, SyntaxR
             return;
         }
         Segment errorSegment = calculateSegmentByLineOfError(file.getText(), annotationResult.getErrorLinePosition());
+        if (EMPTY_SEGMENT == errorSegment) {
+            holder.createErrorAnnotation(TextRange.create(0,1), "tag @startuml not found");
+            // TODO add quick fix for to add @startuml
+        }
+
         String errorMessage = generateErrorMessage(annotationResult);
 
         Annotation annotation = holder.createErrorAnnotation(TextRange.create(errorSegment), errorMessage);
@@ -53,38 +73,14 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<String, SyntaxR
     }
 
     static Segment calculateSegmentByLineOfError(String fileText, int errorLinePosition) {
-        // TODO extract class
-        Scanner scanner = new Scanner(fileText);
-
-        int errorLineStart = -1;
-        int errorLineEnd = -1;
-
-        State state = State.BEFORE_START;
-        int charCountToCurrentLine = 0;
-        int currentLineNumber = 0;
-        int lineWithTagStartuml = -1;
-        while (scanner.hasNext()) {
-            String currentLine = scanner.nextLine();
-            int currentLineLength = currentLine.length() + SIZE_OF_EOL_CHAR;
-            if (state != State.WITHIN_START && currentLine.startsWith("@startuml")) {
-                state = State.WITHIN_START;
-                lineWithTagStartuml = currentLineNumber;
-            }
-            // do something
-            if (state == State.WITHIN_START) {
-                if (lineWithTagStartuml + errorLinePosition == currentLineNumber) {
-                    errorLineStart = charCountToCurrentLine;
-                    errorLineEnd = errorLineStart + currentLineLength;
-                    break;
-                }
-            }
-            charCountToCurrentLine += currentLineLength;
-            currentLineNumber += 1;
+        PumlScanner pumlScanner;
+        try {
+            pumlScanner = new PumlScanner(fileText, errorLinePosition).invoke();
+        } catch (IllegalStateException e) {
+            return EMPTY_SEGMENT;
         }
-
-        if (errorLineStart == -1) {
-            throw new IllegalStateException("Could not calculate error line");
-        }
+        int errorLineStart = pumlScanner.getErrorLineStart();
+        int errorLineEnd = pumlScanner.getErrorLineEnd();
 
         final int finalStart = errorLineStart;
         final int finalEnd = errorLineEnd;
@@ -153,4 +149,59 @@ public class PlantUmlExternalAnnotator extends ExternalAnnotator<String, SyntaxR
     }
 
     static enum State {BEFORE_START, WITHIN_START}
+
+    private static class PumlScanner {
+        private final String fileText;
+        private final int errorLinePosition;
+        private int errorLineStart;
+        private int errorLineEnd;
+
+        public PumlScanner(String fileText, int errorLinePosition) {
+            this.fileText = fileText;
+            this.errorLinePosition = errorLinePosition;
+        }
+
+        public int getErrorLineStart() {
+            return errorLineStart;
+        }
+
+        public int getErrorLineEnd() {
+            return errorLineEnd;
+        }
+
+        public PumlScanner invoke() {
+            Scanner scanner = new Scanner(fileText);
+
+            errorLineStart = -1;
+            errorLineEnd = -1;
+
+            State state = State.BEFORE_START;
+            int charCountToCurrentLine = 0;
+            int currentLineNumber = 0;
+            int lineWithTagStartuml = -1;
+            while (scanner.hasNext()) {
+                String currentLine = scanner.nextLine();
+                int currentLineLength = currentLine.length() + SIZE_OF_EOL_CHAR;
+                if (state != State.WITHIN_START && currentLine.startsWith(TAG_STARTUML)) {
+                    state = State.WITHIN_START;
+                    lineWithTagStartuml = currentLineNumber;
+                }
+                // do something
+                if (state == State.WITHIN_START) {
+                    if (lineWithTagStartuml + errorLinePosition == currentLineNumber) {
+                        errorLineStart = charCountToCurrentLine;
+                        errorLineEnd = errorLineStart + currentLineLength;
+                        break;
+                    }
+                }
+                charCountToCurrentLine += currentLineLength;
+                currentLineNumber += 1;
+            }
+
+            if (errorLineStart == -1) {
+                throw new IllegalStateException("Could not calculate error line");
+            }
+            return this;
+        }
+    }
 }
--- test/de/docksnet/puml/PlantUmlExternalAnnotatorTest.java
+++ test/de/docksnet/puml/PlantUmlExternalAnnotatorTest.java
@@ -7,8 +7,22 @@ import static de.docksnet.puml.PlantUmlExternalAnnotator.*;
 import static org.junit.Assert.assertEquals;
 
 public class PlantUmlExternalAnnotatorTest {
+
     @Test
-    public void test() throws Exception {
+    public void calculateSegmentByLineOfErrorStartOnFirstLine() throws Exception {
+        // given
+        String source = "@startuml\nerror\n@enduml\n";
+
+        // when
+        Segment segment = calculateSegmentByLineOfError(source, 1);
+
+        // then
+        assertEquals(10, segment.getStartOffset());
+        assertEquals(10 + "error\n".length(), segment.getEndOffset());
+    }
+
+    @Test
+    public void calculateSegmentByLineOfErrorStartOnThirdLine() throws Exception {
         // given
         String source = "\nfoo\n@startuml\nerror\n@enduml\n";
 
@@ -18,5 +32,14 @@ public class PlantUmlExternalAnnotatorTest {
         // then
         assertEquals(15, segment.getStartOffset());
         assertEquals(15 + "error\n".length(), segment.getEndOffset());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void calculateSegmentByLineOfErrorNoStart() throws Exception {
+        // given
+        String source = "\nfoo\nerror\n@enduml\n";
+
+        // when
+        calculateSegmentByLineOfError(source, 1);
     }
 }
